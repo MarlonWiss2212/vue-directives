f4d8220e0fe82e1e5c996750b24a84e74823a5ed
mwisskirchen <marlon.wisskirchen@denkwerk.com>
2025-04-08 21:03:06 +0200

    feat: basic setup for plugin

--- build.config.ts
+++ build.config.ts
@@ -4,15 +4,10 @@ export default defineBuildConfig({
   entries: [
     'src/index.ts',
   ],
-
   rollup: {
     emitCJS: true,
   },
-
   clean: true,
-
   sourcemap: false,
-
-  // Generates .d.ts declaration file
   declaration: true,
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from './directives/main'
+import { App } from "vue";
+import { vFocus } from "./directives/focus";
+
+export default {
+  install(app: App) {
+    app.directive('focus', vFocus)
+  }
+}
