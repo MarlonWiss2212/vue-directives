ff1b9f1dc85439045761bc5503ae9c155b4e7014
mwisskirchen <marlon.wisskirchen@denkwerk.com>
2025-04-11 18:21:27 +0200

    feat: add switch case

--- src/utils/install-directives.ts
+++ src/utils/install-directives.ts
@@ -1,6 +1,10 @@
 import type { App } from "vue";
 import { vFocus } from "../directives/focus";
+import { vCase, vDefault, vSwitch } from "../directives/switch";
 
 export function installDirectives(app: App) {
   app.directive('focus', vFocus)
+  app.directive('switch', vSwitch)
+  app.directive('case', vCase)
+  app.directive('default', vDefault)
 }
