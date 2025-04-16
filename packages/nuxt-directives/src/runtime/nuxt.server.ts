759b61a3bfca96caebffc10ace847862995ec7be
mwisskirchen <marlon.wisskirchen@denkwerk.com>
2025-04-16 19:29:47 +0200

    docs(changeset): Fix Nuxt plugin

--- packages/nuxt-directives/src/runtime/nuxt.server.ts
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive('focus', {})
-  nuxtApp.vueApp.directive('switch', {})
-  nuxtApp.vueApp.directive('case', {})
-  nuxtApp.vueApp.directive('default', {})
-})
