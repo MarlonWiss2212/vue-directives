7fda69c26a21d27f9ae80d65fd509917e6cbe26c
mwisskirchen <marlon.wisskirchen@denkwerk.com>
2025-04-17 11:36:04 +0200

    fix: declare nuxt types

--- packages/nuxt-directives/src/runtime/types/directives.d.ts
+++ packages/nuxt-directives/src/runtime/types/directives.d.ts
@@ -1,12 +1,32 @@
-import { vCase, vDefault, vFocus, vSwitch } from "@marlonwiss/vue-directives";
+declare module "#build/directives" {
+  import { vCase, vDefault, vFocus, vSwitch } from "@marlonwiss/vue-directives";
+  export const directives: {
+    vFocus: typeof vFocus;
+    vSwitch: typeof vSwitch;
+    vCase: typeof vCase;
+    vDefault: typeof vDefault;
+  }
+}
 
 declare module 'vue' {
-  interface GlobalDirectives {
+  import { vCase, vDefault, vFocus, vSwitch } from "@marlonwiss/vue-directives";
+  export interface GlobalDirectives {
     vFocus: typeof vFocus;
     vSwitch: typeof vSwitch
     vCase: typeof vCase
     vDefault: typeof vDefault
   }
 }
+
+declare module '@vue/runtime-core' {
+  import { vCase, vDefault, vFocus, vSwitch } from "@marlonwiss/vue-directives";
+  export interface GlobalDirectives {
+    vFocus: typeof vFocus;
+    vSwitch: typeof vSwitch
+    vCase: typeof vCase
+    vDefault: typeof vDefault
+  }
+}
+
 
 export { }
